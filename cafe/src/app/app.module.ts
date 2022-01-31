070dad2568a42dec4b64d8cf0ca81eaf8588affa
Diana <kozachenkodiana03@mail.ru>
2022-01-31 21:46:51 +0700

    Created main components, set up routing

--- cafe/src/app/app.module.ts
+++ cafe/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { WaitingComponent } from './components/waiting/waiting.component';
+import { CheckComponent } from './components/check/check.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WelcomeComponent,
+    RegistrationComponent,
+    MenuComponent,
+    WaitingComponent,
+    CheckComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
